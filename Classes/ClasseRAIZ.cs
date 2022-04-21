9d86370568223655fb1f54e53e1c41ebaa7286e5
maricatto <vmaricatto@gmail.com>
2022-04-21 14:51:15 -0300

    Add files via upload

---
+++ Classes/ClasseRAIZ.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO_CRUDSERIES
+{
+    public abstract class ClasseRAIZ
+    {
+        public int Id { get; protected set; }
+    }
+}
